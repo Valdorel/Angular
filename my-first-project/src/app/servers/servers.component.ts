05e0519be2af2440314a9d86640bef443987d326
Edordo Viganò <edo.vigano.29@gmail.com>
2022-10-02 18:43:45 +0200

    esempio di event binding

--- my-first-project/src/app/servers/servers.component.ts
+++ my-first-project/src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   allowNewServer = false;
-
+  serverCreationStatus = "Nessun server è stato creato";
 
   constructor() {
     setTimeout( () => {
@@ -17,6 +17,10 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Il server è stato creato";
   }
 
 }
