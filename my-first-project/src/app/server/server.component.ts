36752829b8df60236fbad6b94216ad564f898850
Edordo Viganò <edo.vigano.29@gmail.com>
2022-10-02 18:17:43 +0200

    esempio di string interpolation

--- my-first-project/src/app/server/server.component.ts
+++ my-first-project/src/app/server/server.component.ts
@@ -6,4 +6,15 @@ import {Component} from "@angular/core";
 })
 export class ServerComponent {
 
+  serverId = 10;
+  serverStatus = 'offline';
+
+  prova() {
+    return 'server';
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
 }
