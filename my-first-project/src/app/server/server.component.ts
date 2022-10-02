bc92a0ae25b6012c10ae26143a6576b48cabdc4e
Edordo Viganò <edo.vigano.29@gmail.com>
2022-10-02 18:32:59 +0200

    esempio di string interpolation

--- my-first-project/src/app/server/server.component.ts
+++ my-first-project/src/app/server/server.component.ts
@@ -6,6 +6,11 @@ import {Component} from "@angular/core";
 })
 export class ServerComponent {
 
+  /* Per usare la string interpolation bisogna prima definire
+      le variabili nel file component.ts, poi è possibile usare la string interpolation
+      all'interno del component.html
+  * */
+
   serverId = 10;
   serverStatus = 'offline';
 
